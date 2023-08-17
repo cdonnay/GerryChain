d39f35d00addb06088008df193de1c59ad01dc23
Chris Donnay <christopherdonnay@gmail.com>
2023-08-17 10:52:54 -0400

    Update accept.py

--- gerrychain/accept.py
+++ gerrychain/accept.py
@@ -83,3 +83,25 @@ def cut_edge_accept(partition: Partition) -> bool:
         bound = min(1, len(partition.parent["cut_edges"]) / len(partition["cut_edges"]))
 
     return random.random() < bound
+
+def seat_bias_accept(partition: Partition,
+                     election_name: str,
+                    ell: int = 1,
+                 party: str = "Dem") -> bool:
+    """ Implements Metropolis/Hastings Hill climber for number of seats won by party.
+    If the new map increases the number of seats, move there.
+    If it decreases, move with probability proportional to decrease.
+
+    :param ell: float, the metropolis constant, larger values move distribution towards global max/min
+    :param election_name: str, the name of the election updater
+    :param party: str, the name of the party whose seats you increase
+    """
+
+    bound = 1.0
+
+    if partition.parent is not None:
+        parent_score = partition.parent[election_name].seats(party)
+        new_score = partition[election_name].seats(party)
+        bound = min(1,  ell**(new_score-parent_score))
+
+    return random.random() < bound
